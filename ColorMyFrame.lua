0bdfcaac1263d91b29d1c9e7af0ea15229c71b36
JuicyJuuce <juicyjuuce@gmail.com>
2024-06-14 09:30:44 -0500

    removed old/debug code

--- ColorMyFrame.lua
+++ ColorMyFrame.lua
@@ -5,12 +5,8 @@ local f = CreateFrame("Frame")
 
 local defaults = {
 	someOption = true,
-  r = 255/255,
-  g = 200/255,
-  b = 0/255, -- yellow-orange
+  r = 255/255, g = 200/255, b = 0/255, -- yellow-orange
   a = 1.0,
-  --r, g, b = 255/255, 0/255, 255/255, -- magenta
-  --r, g, b = 0/255, 255/255, 0/255, -- green
 }
 
 function f:OnEvent(event, ...)
@@ -18,16 +14,10 @@ function f:OnEvent(event, ...)
 end
 
 function f:myUpdateHealthColor(frame)
-  --if ( UnitPlayerControlled(frame.unit) ) then
   if ( UnitIsUnit(frame.unit, "player") ) then
-    local r, g, b, a = self.db.r, self.db.g, self.db.b, self.db.a
-    --print("r,g,b,a = "..r..", "..g..", "..b..", "..a)
-    --local r, g, b = 255/255, 200/255, 0/255; -- yellow-orange
-    --local r, g, b = 255/255, 0/255, 255/255; -- magenta
-    --local r, g, b = 0/255, 255/255, 0/255; -- green
+    local r, g, b = self.db.r, self.db.g, self.db.b
     if ( r ~= frame.healthBar.r or g ~= frame.healthBar.g or b ~= frame.healthBar.b ) then
       frame.healthBar:SetStatusBarColor(r, g, b);
-      --frame.healthBar.r, frame.healthBar.g, frame.healthBar.b = r, g, b;
     end
   end
 end
@@ -59,25 +49,7 @@ end
 
 f:RegisterEvent("ADDON_LOADED")
 f:RegisterEvent("PLAYER_ENTERING_WORLD")
---f:RegisterEvent("CHAT_MSG_CHANNEL")
 f:SetScript("OnEvent", f.OnEvent)
-
-
---function f:OnEvent(event, addOnName)
---	if addOnName == thisAddonName then
---		ColorMyFrameDB = ColorMyFrameDB or CopyTable(defaults)
---		self.db = ColorMyFrameDB
---		self:InitializeOptions()
---		hooksecurefunc("JumpOrAscendStart", function()
---			if self.db.someOption then
---				print("Your character jumped.")
---			end
---		end)
---	end
---end
---
---f:RegisterEvent("ADDON_LOADED")
---f:SetScript("OnEvent", f.OnEvent)
 
 function f:InitializeOptions()
 	self.optionsPanel = CreateFrame("Frame")
@@ -112,56 +84,30 @@ function f:InitializeOptions()
   end
   setOptionsPanelColor()
 
-  --local r,g,b,a = 1, 0, 0, 1;
-
   function ShowColorPicker(r, g, b, a, changedCallback)
-   --print("r,g,b,a = "..r..", "..g..", "..b..", "..a)
    local info = {}
    info.hasOpacity, info.opacity = (a ~= nil), a;
    info.r, info.g, info.b = r, g, b
    info.swatchFunc, info.func, info.opacityFunc, info.cancelFunc = changedCallback, changedCallback, changedCallback, changedCallback;
-   --ColorPickerFrame:SetColorRGB(r,g,b);
-   --ColorPickerFrame:Hide(); -- Need to run the OnShow handler.
-   --ColorPickerFrame:Show();
    ColorPickerFrame:SetupColorPickerAndShow(info)
   end
 
   local function myColorCallback(restore)
     local newR, newG, newB, newA;
     if restore then
-      --print("restore:")
-      --for key, value in pairs(restore) do
-      --  print(key, value)
-      --end
-      -- The user bailed, we extract the old color from the table created by ShowColorPicker.
-      newR, newG, newB, newA = restore.r, restore.g, restore.b, restore.a --unpack(restore);
-      --print("if restore TRUE")
+      newR, newG, newB, newA = restore.r, restore.g, restore.b, restore.a
     else
       -- Something changed
       newA, newR, newG, newB = ColorPickerFrame:GetColorAlpha(), ColorPickerFrame:GetColorRGB();
-      --print("if restore FALSE")
     end
-    --print("newR = "..newR)
-    --print("newG = "..newG)
-    --print("newB = "..newB)
-    --print("newA = "..newA)
-    --if newA then
-    --  --print("newR,newG,newB,newA = "..newR..", "..newG..", "..newB..", "..newA)
-    --else
-    --  print("newA is nil")
-    --end
-   
+
     -- Update our internal storage.
     self.db.r, self.db.g, self.db.b, self.db.a = newR, newG, newB, newA;
     setOptionsPanelColor()
     -- And update any UI elements that use this color...
-    --CompactPartyFrameMixin:UpdateLayout() --CompactUnitFrame_UpdateAll(CompactPartyFrame)
-    --RaidPullout_RenewFrames()
     CompactRaidFrameContainerMixin:TryUpdate()
   end
   
-  --ShowColorPicker(self.db.r, self.db.g, self.db.b, self.db.a, myColorCallback);
-
 	btn:SetScript("OnClick", function()
 		print("You clicked me!")
     ShowColorPicker(self.db.r, self.db.g, self.db.b, self.db.a, myColorCallback);
@@ -177,22 +123,7 @@ SlashCmdList.CMF = function(msg, editBox)
 	InterfaceOptionsFrame_OpenToCategory(f.optionsPanel)
 end
 
-
---local optionsPanel = CreateFrame("Frame")
---optionsPanel.name = thisAddonTitle
---InterfaceOptions_AddCategory(optionsPanel)
-
---local title = optionsPanel:CreateFontString("ARTWORK", nil, "GameFontNormalLarge")
---title:SetPoint("TOPLEFT")
---title:SetText(thisAddonTitle)
-
---local footer = optionsPanel:CreateFontString("ARTWORK", nil, "GameFontNormal")
---footer:SetPoint("TOPLEFT", 0, 0)
---footer:SetText("This is 5000 below the top, so the optionsPanel automatically expanded.")
-
 function ColorMyFrame_OnAddonCompartmentClick(addonName, buttonName, menuButtonFrame)
   print("Hello from the addon compartment, the '"..addonName.."' addon was clicked with "..buttonName)
-  --InterfaceOptionsFrame_Show()
   InterfaceOptionsFrame_OpenToCategory(f.optionsPanel)
 end
- 
